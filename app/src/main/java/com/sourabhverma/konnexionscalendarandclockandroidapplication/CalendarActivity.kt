8caa17e4c1f6e04916be44234036c7603606f46b
SOURABH KUMAR VERMA <1906286@kiit.ac.in>
2021-11-14 15:02:50 +0530

    Added Calendar and Clock Activity

---
+++ app/src/main/java/com/sourabhverma/konnexionscalendarandclockandroidapplication/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.sourabhverma.konnexionscalendarandclockandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
---
+++ app/src/main/java/com/sourabhverma/konnexionscalendarandclockandroidapplication/ClockActivity.kt
@@ -0,0 +1,11 @@
+package com.sourabhverma.konnexionscalendarandclockandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clock)
+    }
+}
